1eb3f7208cfcd149d8cff0102d4e13e0afd60ff9
Vincent <rumble.vecent@outlook.com>
2021-07-05 22:47:22 +0800

    Add GaiaRemoteCall.

--- SetupInstallerScripts.sh
+++ SetupInstallerScripts.sh
@@ -3,3 +3,5 @@ chmod +x GaiaInstall
 cp ./GaiaInstall /usr/local/bin/
 chmod +x GaiaRemoteInstall
 cp ./GaiaRemoteInstall /usr/local/bin/
+chmod +x GaiaRemoteCall
+cp ./GaiaRemoteCall /usr/local/bin
